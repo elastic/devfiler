b077c1aeeddc239482576a439811b9e5ccd52098
Florian Lehner <florian.lehner@elastic.co>
2025-07-18 15:00:07 +0200

    nix: update to 2025-05
    Signed-off-by: Florian Lehner <florian.lehner@elastic.co>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "devfiler: universal profiling as a desktop app";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
     crane.url = "github:ipetkov/crane";
     crane.inputs.nixpkgs.follows = "nixpkgs";
